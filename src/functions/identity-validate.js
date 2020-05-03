977c16560d2ba05f0b3f92ab55495423256d9291
Raghav Sethi <raghavsethi.rs@gmail.com>
2020-05-03 15:32:02 -0700

    Factor out shared code

--- src/functions/identity-validate.js
+++ src/functions/identity-validate.js
@@ -1,54 +1,8 @@
-const Airtable = require("airtable");
-
-const airtableApiKey = process.env.AIRTABLE_API_KEY;
-const airtableBaseId = process.env.AIRTABLE_BASE_ID;
-const airtablePrimaryTableName = process.env.AIRTABLE_PRIMARY_TABLE_NAME;
-
-const airtablePrimaryTableViewName =
-  process.env.AIRTABLE_PRIMARY_TABLE_VIEW_NAME;
-
-const base = new Airtable({ apiKey: airtableApiKey }).base(airtableBaseId);
-
-const getMatchingUserProfilesAsync = async function(email) {
-  const completionPromise = new Promise((resolve, reject) => {
-    const matchingRecords = [];
-    const filter = {
-      filterByFormula: `{Email} = "${email}"`,
-      ...(airtablePrimaryTableViewName
-        ? { view: airtablePrimaryTableViewName }
-        : {})
-    };
-    base(airtablePrimaryTableName)
-      .select(filter)
-      .eachPage(
-        (records, fetchNextPage) => {
-          matchingRecords.push(...records);
-          fetchNextPage();
-        },
-        err => {
-          if (err) {
-            console.error(err);
-            reject(err);
-            return;
-          }
-
-          resolve(matchingRecords);
-        }
-      );
-  });
-
-  const results = await completionPromise;
-  return results.map(r => r.fields);
-};
-
-const checkUserAuthorizedAsync = async function(email) {
-  const matchingProfiles = await getMatchingUserProfilesAsync(email);
-  return matchingProfiles.length > 0;
-};
+const airtableHelpers = require("./shared");
 
 exports.handler = async (event, context) => {
   const parsedEvent = JSON.parse(event.body);
-  const allowUserAccess = await checkUserAuthorizedAsync(
+  const allowUserAccess = await airtableHelpers.checkUserAuthorizedAsync(
     parsedEvent.user.email
   );
   return {
